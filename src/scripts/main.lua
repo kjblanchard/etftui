971ff07450ff7213d4f829ab061cde01571469fe
Kevin Blanchard <blanchardkevinj@gmail.com>
2025-10-24 11:27:42 -0400

    add in gameobject, and moving

--- src/scripts/main.lua
+++ src/scripts/main.lua
@@ -1,25 +1,57 @@
 local engine = require("Engine")
 local config = require("gameConfig")
-local shader = nil
-local texture = nil
+local gameState = require("gameState")
 if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
     require("lldebugger").start()
 end
 
+local player = {
+    playerGO = nil,
+    x = 0,
+    y = 0,
+    playerSprite = nil,
+    moveSpeed = 100
+}
+
 
 local function handleInput()
-    -- engine.Input.Update()
+end
+
+local function playerInput()
+    local moved = false
+    local velocityX = 0
+    local velocityY = 0
+    if engine.Input.KeyboardKeyDown(engine.Input.Buttons.UP) then
+        velocityY = -1
+        moved = true
+    end
+    if engine.Input.KeyboardKeyDown(engine.Input.Buttons.RIGHT) then
+        velocityX = 1
+        moved = true
+    end
+    if engine.Input.KeyboardKeyDown(engine.Input.Buttons.LEFT) then
+        velocityX = -1
+        moved = true
+    end
+    if engine.Input.KeyboardKeyDown(engine.Input.Buttons.DOWN) then
+        velocityY = 1
+        moved = true
+    end
+    if moved then
+        local delta = gameState.DeltaTimeSeconds
+        player.x = player.x + velocityX * player.moveSpeed * delta
+        player.y = player.y + velocityY * player.moveSpeed * delta
+        engine.Gameobject.SetPosition(player.playerGO, player.x, player.y)
+    end
 end
 
 
 local function update()
-    -- engine.EngineUpdate()
+    engine.EngineUpdate()
+    playerInput()
 end
 
 local function draw()
-    if texture and shader then
-        engine.DrawTexture(texture, shader, { x = 0, y = 0, w = 48, h = 48 }, { x = 0, y = 0, w = 48, h = 48 })
-    end
 end
 
 
@@ -30,5 +62,6 @@ engine.SetDrawFunc(draw)
 engine.Audio.SetGlobalBGMVolume(config.audio.bgmVolume)
 engine.Audio.SetGlobalSFXVolume(config.audio.sfxVolume)
 engine.Audio.PlayBGM("town2")
-shader = engine.CreateShader("2dSpriteVertex", "2dSpriteFragment")
-texture = engine.CreateTexture("test")
+player.playerGO = engine.Gameobject.CreateGameObject()
+engine.Gameobject.SetPosition(player.playerGO, 40, 40)
+player.playerSprite = engine.Sprite.NewSprite("test", player.playerGO, { 0, 0, 48, 48 }, { 0, 0, 48, 48 })
